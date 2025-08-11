98b8b297f2c9ab7a046dde29c3c0ba8ba3cbd3b2
William <pedro.williamsoares2022@gmail.com>
2025-08-11 20:48:05 -0300

    Implementa validação de nomes duplicados e campo vazio ao adicionar amigo, garante sorteio válido sem pares iguais, e adiciona função de reiniciar lista e resultado.

--- app.js
+++ app.js
@@ -41,29 +41,33 @@ function atualizarLista() {
   }
 }
 
-function sortearAmigo() {
+// Essa função retorna true se ninguém tirou a si mesma
+function sorteioValido(listaOriginal, listaSorteada) {
+  for(let i =0; i < listaOriginal.length; i++ ){
+    if(listaOriginal[i] === listaSorteada[i]){
+      return false; //sorteio é inválido
+    }
+  }
+  return true; // sortei válido
+}
 
+function sortearAmigo() {
   if (listaNomes.length === 0) {
     alert("A lista de amigos está vazia")
   } else if (listaNomes.length < 4) {
     alert("Para sortear é necessário ter no mínimo 4 amigos na lista");
   } else {
-    let resultado = document.getElementById('resultado');
-    let listaEmbaralhada = [...listaNomes];
-
-    for (let i = listaEmbaralhada.length - 1; i > 0; i--) {
-      let sorteado = Math.floor(Math.random() * (i + 1));
-      let temp = listaEmbaralhada[i];
-      listaEmbaralhada[i] = listaEmbaralhada[sorteado];
-      listaEmbaralhada[sorteado] = temp;
-    }
-    for (let i = 0; i < listaNomes.length; i++) {
-      if (listaNomes[i] === listaEmbaralhada[i]) {
-        let tempList = listaEmbaralhada[i];
-        listaEmbaralhada[i] = listaEmbaralhada[listaEmbaralhada.length - 1];
-        listaEmbaralhada[listaEmbaralhada.length - 1] = tempList;
+    let listaEmbaralhada;
+    do {
+      listaEmbaralhada = [...listaNomes];
+      for (let i = listaEmbaralhada.length - 1; i > 0; i--) {
+        let sorteado = Math.floor(Math.random() * (i + 1));
+        let temp = listaEmbaralhada[i];
+        listaEmbaralhada[i] = listaEmbaralhada[sorteado];
+        listaEmbaralhada[sorteado] = temp;
       }
-    }
+    } while(!sorteioValido(listaNomes, listaEmbaralhada));
+    let resultado = document.getElementById('resultado');
     resultado.innerHTML = '';
     for (let i = 0; i < listaNomes.length; i++) {
       resultado.innerHTML += `<li>${listaNomes[i]} tirou ${listaEmbaralhada[i]}</li>`;
@@ -80,4 +84,3 @@ function reiniciar() {
 
 let botaoReiniciar = document.getElementById('btnReiniciar');
 botaoReiniciar.addEventListener('click', reiniciar);
-
